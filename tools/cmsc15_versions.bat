9c63e875c89f6622f706c147001c989c090594c5
Mladen Turk <mturk@apache.org>
2020-11-29 20:08:48 +0100

    Update versions

--- tools/cmsc15_versions.bat
+++ tools/cmsc15_versions.bat
@@ -14,11 +14,11 @@ rem limitations under the License.
 rem
 rem Setup environment for Microsoft Compiler Toolkit
 rem
-set "CmscVer=15.0_33_dev"
+set "CmscVer=15.0_33"
 set "CmscSys=64"
 set "CmscOsv=win7"
 set "NasmVer=2.15.05"
 set "PerlVer=5.32.0.1"
-set "P2wxVer=2.0.4"
+set "P2wxVer=2.0.5"
 set "CurlOpts=-qkL --retry 5"
 set "WINVER=0x0601"
