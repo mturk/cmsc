2c77aecf5437c9a605ac134fa73798768b766419
Mladen Turk <mturk@apache.org>
2021-10-13 07:16:30 +0200

    Update cygwrun dependency and bump version

--- tools/cmsc15_versions.bat
+++ tools/cmsc15_versions.bat
@@ -14,11 +14,11 @@ rem limitations under the License.
 rem
 rem Setup environment for Microsoft Compiler Toolkit
 rem
-set "CmscVer=15.0_38"
+set "CmscVer=15.0_39"
 set "CmscSys=64"
 set "CmscOsv=win7"
 set "NasmVer=2.15.05"
 set "PerlVer=5.32.1.1"
-set "CygwrunVer=1.0.2"
+set "CygwrunVer=1.0.3"
 set "CurlOpts=-qkL --retry 5"
 set "WINVER=0x0601"
